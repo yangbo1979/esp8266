41c5edd4465bc2555ef8bf486c93ae7a79b7234b
yangbo1979 <gyangbo@gmail.com>
2015-02-06 14:45:22 +0800

    Update network_default_cfg.lua

--- EasyWebConfig/network_default_cfg.lua
+++ EasyWebConfig/network_default_cfg.lua
@@ -1,6 +1,8 @@
 ssid="ESP8266_".. node.chipid()
 password="12345678"
 
+_G["para"] = {}
+
 wifi.setmode(wifi.SOFTAP)
 --set ap ssid and pwd
 cfg={}
@@ -15,27 +17,33 @@ srv:listen(80,function(conn)
      conn:on("receive",function(conn,payload)
           --print(payload)
           print("received")
-          --conn:send("<h1> Hello, NodeMcu.</h1>")
+          --find last line in plyload(stupid function,improve later)
+          local i = 0
+          local j = 0
+          while true do
+               i = string.find(payload, "\n", i+1)
+               -- find 'next' newline  
+               if i == nil then break
+               else 
+               j=i
+               end    
+          end
+          paraStr = string.sub(payload,j)
+          
           if(_G["wifiStatue"] == nil) then _G["wifiStatue"]="..." end
-          k = string.find(payload,"ssid")
-          if k then
-               str=string.sub(payload,k)
-               --print(str)
-               m=string.find(str,"&")
-               --print(m)
-               strssid='ssid='..'"'..string.sub(str,6,(m-1))..'"'
-               print(strssid)
-               strpass='password="'..string.sub(str,(m+10))..'"'
-               print(strpass)
 
+          --there should be a "=" in Post data,such as ssid=id&password=ps
+          if (string.find(paraStr,"=")~=nil) then
                file.open("network_user_cfg.lua","w+")
-               file.writeline(strssid)
-               file.writeline(strpass)
-               file.close()
-
+               for name, value in string.gfind(paraStr, "([^&=]+)=([^&=]+)") do
+                 file.writeline(name.."=\""..value.."\"")
+               end
+               
                _G["wifiStatue"]="Saved"
                print("store ok")
+               file.close()
           end
+
           -- html-output
           conn:send("HTTP/1.0 200 OK\r\nContent-type: text/html\r\nServer: ESP8266\r\n\n")
           conn:send("<html><head>")
@@ -50,11 +58,14 @@ srv:listen(80,function(conn)
           end
           conn:send("<FORM action=\"\" method=\"POST\">")
           conn:send("<table><tr><td>")
-          conn:send("SSID:</td><td><input type=\"text\" name=\"ssid\" value=\"")
-          if(_G["wifissid"] ~= nil) then conn:send(_G["wifissid"]) end
+          
+          for vK,vN in ipairs(_G["config"]) do
+          conn:send("<tr><td>"..vN.name.."</td><td><input type=\"text\" name=\""..vN.name.."\" value=\"")
+          if(_G[vN.name] ~= nil) then 
+          conn:send(_G[vN.name])
+          end
           conn:send("\"></td></tr>")
-          conn:send("<tr><td>")
-          conn:send("Password</td><td><input type=\"password\" name=\"password\"></td></tr>")
+          end
           conn:send("<tr><td><input type=\"submit\" value=\"SAVE\"></td></tr>")
           conn:send("</table>")
           conn:send("</form></div>")
@@ -63,8 +74,8 @@ srv:listen(80,function(conn)
           conn:close()
 
           if(_G["wifiStatue"]=="Saved") then
-               print("try to reboot")
-               tmr.alarm(0,5000,0,function()node.restart() end )
+               print("reboot")
+               tmr.alarm(0,3000,0,function()node.restart() end )
           elseif(_G["wifiStatue"]=="..." or _G["wifiStatue"]=="Failed") then 
                --keep server open for 10 min to configure
                tmr.alarm(0,600000,0,function()node.restart() end )
