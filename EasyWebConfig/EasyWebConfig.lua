3c4dbb8a6111f53a94cfc7342c727cf81998e3cd
yangbo1979 <gyangbo@gmail.com>
2015-02-13 17:39:19 +0800

    update

--- EasyWebConfig/EasyWebConfig.lua
+++ EasyWebConfig/EasyWebConfig.lua
@@ -5,7 +5,7 @@
 --------------------------------------------------------------------------------
 
 --[[
-here is the init.lua:
+here is the demo.lua:
 require("EasyWebConfig")
 --EasyWebConfig.addVar("gateWay")
 --EasyWebConfig.addVar("userKey")
@@ -50,7 +50,7 @@ if( file.open("network_user_cfg.lua") ~= nil) then
                     tmr.stop(1)
                     if (cnt < 10) then print("IP:"..wifi.sta.getip())
                          --_G["wifiStatue"] = "OK"
-                         node.led(0,0)
+                         --node.led(0,0)
                          if(userScriptFile ~="") then 
                               --print(node.heap())
                               --for n in pairs(_G) do print(n) end
@@ -65,7 +65,7 @@ if( file.open("network_user_cfg.lua") ~= nil) then
                          end
                     else print("FailToConnect,LoadDefault")
                          _G["wifiStatue"] = "Failed"
-                         node.led(800,50)
+                         --node.led(800,50)
                          _G["wifissid"] = ssid
                          require("network_default_cfg")
                          print ("LoadDefault")
@@ -76,5 +76,5 @@ if( file.open("network_user_cfg.lua") ~= nil) then
 else--not exist user config file,use default one
      require("network_default_cfg")
      print ("LoadDefault")
-     node.led(800,800)
+     --node.led(800,800)
 end
